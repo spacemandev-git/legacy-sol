a9087cd5bd8734722e07d196b29cb6b2eb1ca6ce
spacemandev <devbharel@gmail.com>
2022-01-03 22:28:55 -0600

    game accounts being created

--- programs/legacy-sol/src/lib.rs
+++ programs/legacy-sol/src/lib.rs
@@ -1,22 +1,35 @@
 use anchor_lang::prelude::*;
 //use std::collections::HashMap;
+mod errors;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod legacy_sol {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, bump:u8) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, _bump:u8) -> ProgramResult {
         let admin_acc = &mut ctx.accounts.admin_account;
-        admin_acc.admin = ctx.accounts.admin.key();
+        admin_acc.key = ctx.accounts.admin.key();
         Ok(())
+    }
+
+    pub fn create_game(ctx: Context<InitGame>, id:String, _bump:u8, admin_pk: Pubkey) -> ProgramResult {
+        if ctx.accounts.admin.key() != ctx.accounts.admin_account.key {
+            return Err(errors::NotAuthorizedError::Unauthorized.into())
+        } else {
+            let game_account = &mut ctx.accounts.game_account;
+            game_account.enabled = true; //TODO: Default to False and then change it via functions. For debug purposes we'll just enable the game
+            game_account.admin = admin_pk;
+            game_account.id = id;
+            Ok(())
+        }
     }
 }
 
 #[derive(Accounts)]
-#[instruction(bump: u8)]
+#[instruction(_bump: u8)]
 pub struct Initialize<'info> {
-    #[account(init, seeds=[admin.key().as_ref()], bump=bump, payer=admin, space=8+32)]
+    #[account(init, seeds=[admin.key().as_ref()], bump=_bump, payer=admin, space=8+32)]
     pub admin_account: Account<'info, AdminAccount>,
     pub admin: Signer<'info>,
     pub system_program: Program<'info, System>
@@ -24,24 +37,23 @@ pub struct Initialize<'info> {
 
 #[account]
 pub struct AdminAccount {
-    admin: Pubkey,
+    key: Pubkey,
 }
 
-/*
-#[account]
-pub struct GameAccount<'info> {
-    id: String,
-    admin: Signer<'info>,
-    enabled: bool,
+#[derive(Accounts)]
+#[instruction(id: String, _bump:u8, admin_pk: Pubkey)]
+pub struct InitGame<'info> {
+    pub admin_account: Account<'info, AdminAccount>,
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    #[account(init, seeds=[id.as_ref()], bump=_bump, payer=admin, space=8+32+58+1)]
+    pub game_account: Account<'info, GameAccount>,
 }
 
 #[account]
 #[derive(Default)]
-pub struct Map {
-    coords: HashMap<(i64,i64), u64>,
-    x_min: i64,
-    x_max: i64,
-    y_min: i64,
-    y_max: i64,
+pub struct GameAccount {
+    id: String,
+    admin: Pubkey,
+    enabled: bool,
 }
-*/
--- tests/legacy-sol.ts
+++ tests/legacy-sol.ts
@@ -1,10 +1,11 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
+import { assert } from 'chai';
 import { LegacySol } from '../target/types/legacy_sol';
 
 const {SystemProgram} = anchor.web3;
 
-describe('legacy-sol', () => {
+describe('Setup legacy game', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
@@ -12,6 +13,7 @@ describe('legacy-sol', () => {
   //@ts-ignore
   const program = anchor.workspace.LegacySol as Program<LegacySol>;
   const provider = anchor.Provider.env();
+
 
   it('Is initialized!', async () => {
     // Add your test here.
@@ -23,9 +25,42 @@ describe('legacy-sol', () => {
         systemProgram: SystemProgram.programId
       }
     });
-    console.log("Initalization Admin Account: ", await program.account.adminAccount.fetch(systemacc.account));
-
+    console.log("Initalization Admin Account:", await program.account.adminAccount.fetch(systemacc.account));
   });
+
+  const gameId = "101";
+  it("Only admin can create a game", async () => {
+    const systemacc = await getPDA(provider.wallet.publicKey.toBuffer(), program.programId);
+    const gameacc = await getPDA(Buffer.from(gameId), program.programId);
+    //console.log("Initalization Admin Account:", await program.account.adminAccount.fetch(systemacc.account));
+    const kp = anchor.web3.Keypair.generate();
+    program.rpc.createGame(gameId, gameacc.bump, provider.wallet.publicKey, {
+      accounts: {
+        adminAccount: systemacc.account,
+        admin: kp.publicKey,
+        systemProgram: SystemProgram.programId,
+        gameAccount: gameacc.account
+      },
+      signers: [kp]
+    })
+    .then(val => {throw new Error("Did not error correctly!");})
+    .catch(err => {console.log("Errors Correctly!");})
+  }) 
+
+  it("Creates a game as admin", async () => {
+    const systemacc = await getPDA(provider.wallet.publicKey.toBuffer(), program.programId);
+    const gameacc = await getPDA(Buffer.from(gameId), program.programId);
+
+    await program.rpc.createGame(gameId, gameacc.bump, provider.wallet.publicKey, {
+      accounts: {
+        adminAccount: systemacc.account,
+        admin: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+        gameAccount: gameacc.account
+      }
+    })
+    console.log(await program.account.gameAccount.fetch(gameacc.account));    
+  })
 });
 
 const getPDA = async (accBuf: Buffer, programId: anchor.web3.PublicKey) => {
